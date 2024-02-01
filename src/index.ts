753e5fac5cc983e0ffc837ae3152eb0a748db182
AdienChen <565302968@qq.com>
2024-02-02 00:23:07 +0800

    feat: 添加类型支持

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import fs from "fs";
-import path from "path";
 import { initCli } from "./cli/index";
 import { StandardConfig } from "./config";
 
+export { StandardConfig } from "./config";
 export function main() {
   initCli();
 }
--- tsup.config.ts
+++ tsup.config.ts
@@ -5,5 +5,6 @@ export default defineConfig({
   sourcemap: false,
   treeshake: true,
   clean: true,
-  format: "esm",
+  dts: true,
+  format: ["esm", "cjs"],
 });
